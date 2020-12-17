67ad6b3ad8ec0efb36309f3c0f3cef7db5acaf67
Ramaf <ramafauzifa@gmail.com>
2020-12-17 22:02:59 +0700

    Query User dan DBConfig

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,26 @@
 <?php
 
-class User
+class User extends Model
 {
-    
+    protected $id;
+    protected $nama;
+    protected $username;
+
+    public function __construct($Id, $Nama, $Username){
+        $this->id = $id;
+        $this->nama = $nama;
+        $this->username = $username;
+    }
+
+    public function get_id(){
+        return $this->id;
+    }
+
+    public function get_nama(){
+        return $this->nama;
+    }
+
+    public function get_username(){
+        return $this->username;
+    }
 }
