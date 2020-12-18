5cbe817130c6698eae7530623e9db70ae0753369
Juan Anthonius Kusjadi <75063337+JuanAnthoniusKusjadi@users.noreply.github.com>
2020-12-19 00:58:58 +0700

    Update SQL (#5)
    * sql table pasien
    
    * sql table patientcase
    
    * sql table patientinfected
    
    * sql berisi nama kota
    
    * sql berisi negara asal pasien covid
    
    * sql berisi nama provinsi
    
    * sql berisi state dari pasien covid
    
    * sql berisi jumlah orng terkonfirmasi covid
    
    * sql table caseinfection
    
    * sql table searchtrend
    
    * sql yang berisi jumlah infection per tanggal
    
    * sql berisi jumlah infek sesuai gender per tanggal
    
    * sql berisi jumlah infeksi sesuai gender per tanggal
    
    * sql yang berisi jumlah infeksi per provinsi per tanggal
    
    * hapus
    
    * Sql untuk data covid19 Korsel
    
    * Sql data covid part 2
    
    * database city, country, province dan state
    
    * Dataset time
    
    * Update
    
    * Query User dan DBConfig
    
    Co-authored-by: SyahdanRiyantyoPutro <6181801043@student.unpar.ac.id>
    Co-authored-by: Ramaf <ramafauzifa@gmail.com>
    Co-authored-by: Shannas Rizqi Raihan <shannasr12@gmail.com>
    Co-authored-by: EdwardTjahyadi <edtjah16@gmail.com>

--- app/models/QueryUser.php
+++ app/models/QueryUser.php
@@ -11,6 +11,160 @@ class QueryUser extends Model
         $this->db = new DB();
     }
 
+    public function get_all_user(){
+        $query = '
+            SELECT 
+                id, nama, username, password
+            FROM 
+                User
+        ';
+
+        $queryResult = $this->db->executeSelectQuery($query);
+        $result = [];
+        foreach ($queryResult as $key => $value) {
+            $result[] = new User($value['id'], $value['nama'], $value['username']);
+        }
+
+        return $result;
+    }
+
+    public function get_user_by_id($id) {
+        $query = '
+            SELECT
+                id, nama, username, password
+            FROM
+                User
+            WHERE
+                id = '. $id .'
+        ';
+
+        $queryResult = $this->db->executeSelectQuery($query);
+
+        return new User($queryResult[0]['id'], $queryResult[0]['nama'], $queryResult[0]['username']);
+    }
+
+
+    public function create_user($nama, $username)
+    {
+        $nama = $this->db->escapeString($nama);
+        $username = $this->db->escapeString($username);
+        $password = $this->db->escapeString($password);
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "
+            INSERT INTO User (
+                nama, username, password
+            )
+            VALUES (
+                '$nama', '$username', '$hashed_password'
+            )
+        ";
+
+        $query_result = $this->db->executeNonSelectQuery($query);
+
+        if (!$query_result) {
+            $this->error = $this->db->get_error();
+            return false;
+        }
+
+        return true;
+    }
+
+    public function delete_user($id, $username)
+    {
+        $user = $this->get_user_by_id($id);
+        $id = $this->db->escapeString($id);
+        $username = $this->db->escapeString($username);
+
+        $query = "
+            DELETE FROM User
+            WHERE id = '$id' AND username = '$username'
+        ";
+
+        $query_result = $this->db->executeNonSelectQuery($query);
+
+        if (!$query_result) {
+            $this->error = $this->db->get_error();
+            return false;
+        }
+
+        $file_path = ROOT . 'public' . DS . 'uploads' . DS . $user->get_profile_path();
+
+        if(file_exists($file_path)) {
+            unlink($file_path);
+        }
+
+        return true;
+    }
+
+    public function update_user($id, $username = null, $nama = null)
+    {
+        $id = $this->db->escapeString($id);
+        $nama = $this->db->escapeString($nama);
+        $username = $this->db->escapeString($username);
+        $password = $this->db->escapeString($password);
+
+        $query = "
+            UPDATE User
+            SET
+        ";
+        if ($username != null) {
+            $query .= " username = '$username',";
+        }
+        if ($nama != null) {
+            $query .= " nama = '$nama',";
+        }
+        if ($password != null) {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $query .= " password = '$hashed_password',";
+        }
+        $query = substr($query, 0, -1);
+        $query .= " WHERE id = '$id'";
+
+        $query_result = $this->db->executeNonSelectQuery($query);
+
+        if (!$query_result) {
+            $this->error = $this->db->get_error();
+            return false;
+        }
+
+        return true;
+    }
+
+    public function read_user($username, $password)
+    {
+        $username = $this->db->escapeString($username);
+        $password = $this->db->escapeString($password);
+
+        $query = "
+            SELECT
+                id, username, password
+            FROM 
+                User
+            WHERE   
+                username = '$username'
+        ";
+
+        $query_result = $this->db->executeSelectQuery($query);
+
+        if ($this->db->get_error() != null) {
+            $this->error = $this->db->get_error();
+            return false;
+        } else {
+            if (count($query_result) != 0) {
+                if (password_verify($password, $query_result[0]['password'])) {
+                    return new User($query_result[0]['id'], $query_result[0]['username'], $query_result[0]['tipe'], $query_result[0]['username'], $query_result[0]['last_login'], $query_result[0]['profile_location']);
+                } else {
+                    $this->error = 'user or password incorrect';
+                    return false;
+                }
+            } else {
+                $this->error = 'user or password incorrect';
+                return false;
+            }
+        }
+    }
+
     public function get_error() {
         return $this->error;
     }
