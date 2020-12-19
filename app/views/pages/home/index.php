32e632e74b8da20c00fe8773d280b7d6947a1bc7
DeddyChandra <6181801007@student.unpar.ac.id>
2020-12-19 09:55:10 +0700

    css home

--- app/views/pages/home/index.php
+++ app/views/pages/home/index.php
@@ -1,33 +1,37 @@
 <div class="container py-4 my-4 min-vh-100">
-    <div id="demo" class="carousel slide" data-ride="carousel">
+    <div class="row">
+        <div class="col-sm ">
+            <div id="carous" class="carousel slide rounded" data-ride="carousel" width="50%">
+            <!-- Indicators -->
+            <ul class="carousel-indicators">
+            <li data-target="#carous" data-slide-to="0" class="active"></li>
+            <li data-target="#carous" data-slide-to="1"></li>
+            <li data-target="#carous" data-slide-to="2"></li>
+            </ul>
 
-    <!-- Indicators -->
-    <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-    </ul>
+            <!-- The slideshow -->
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?= IMG_PATH . "pic1.jpg"; ?>" alt="Los Angeles" width="100%">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= IMG_PATH . "pic2.jpg"; ?>" alt="Chicago" width="100%">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= IMG_PATH . "pic3.jpg"; ?>" alt="New York" width="100%">
+            </div>
+            </div>
 
-    <!-- The slideshow -->
-    <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img src="<?= IMG_PATH . "pic1.jpg"; ?>" alt="Los Angeles" width="100%">
-    </div>
-    <div class="carousel-item">
-        <img src="<?= IMG_PATH . "pic2.jpg"; ?>" alt="Chicago" width="100%">
-    </div>
-    <div class="carousel-item">
-        <img src="<?= IMG_PATH . "pic3.jpg"; ?>" alt="New York" width="100%">
-    </div>
-    </div>
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#carous" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#carous" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+            </a>
 
-    <!-- Left and right controls -->
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-    </a>
-
+            </div>
+        </div>
+        <div class="col-sm"></div>
     </div>
 </div>
