d75d8591f1598991fae8364de5a9a055f690b47f
JuanAnthoniusKusjadi <juan_20200249@unpar.ac.id>
2020-12-22 15:01:17 +0700

    repair queryPatient and add to page

--- app/models/QueryPatient.php
+++ app/models/QueryPatient.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once MODEL_PATH . 'Patient.php';
-class QueryUser extends Model
+class QueryPatient extends Model
 {
     protected $db;
     protected $error = null;
@@ -11,28 +11,77 @@ class QueryUser extends Model
         $this->db = new DB();
     }
 
-    public function get_all_patient(){
+    public function get_error() 
+    {
+        return $this->error;
+    }
+
+    public function get_count_all_patient() 
+    {
         $query = '
             SELECT 
-                patient_id, sex, age, country, province, city, state
-            FROM 
-                patient
-        ';'
-            SELECT
-                infected_by
-            FROM
-                infectedBy
-        ';'
-            SELECT
-                infection_case, sympton_onset_date, confirmed_date, released_date, deceased_date
-            FROM
-                infectionCase
+                COUNT(`patient_id`) as `count`
+            FROM `patient`
+        ';
+
+        $queryResult = $this->db->executeSelectQuery($query);
+        
+        $result = $queryResult[0]['count'];
+
+        return $result;
+    }
+
+    public function get_all_patient($start = 0, $end = 10)
+    {
+        $query = '
+            SELECT 
+                `patient`.`patient_id`,
+                `patient`.`sex`,
+                `patient`.`age`,
+                `country`.`country`,
+                `province`.`province`,
+                `city`.`city`,
+                `state`.`state`,
+                `patientinfected`.`infected_by`,
+                `patientcase`.`infection_case`,
+                `patientcase`.`sympton_onset_date`,
+                `patientcase`.`confirmed_date`,
+                `patientcase`.`released_date`,
+                `patientcase`.`deceased_date`
+            FROM `patient` 
+                INNER JOIN `patientcase` 
+                    ON `patient`.`patient_id` = `patientcase`.`patient_id` 
+                INNER JOIN `patientinfected` 
+                    ON `patient`.`patient_id` = `patientinfected`.`patient_id` 
+                INNER JOIN `province` 
+                    ON `patient`.`province` = `province`.`idProvince` 
+                INNER JOIN `state` 
+                    ON `patient`.`state` = `state`.`idState` 
+                INNER JOIN `city` 
+                    ON `patient`.`city` = `city`.`idCity` 
+                INNER JOIN `country` 
+                    ON `patient`.`country` = `country`.`idCountry`
+            LIMIT '. $start .','. $end .'
         ';
 
         $queryResult = $this->db->executeSelectQuery($query);
         $result = [];
         foreach ($queryResult as $key => $value) {
-            $result[] = new Patient($value['id'], $value['gender'], $value['age'], $value['nationality'], $value['province'], $value['city'], $value['infectionCase'], $value['infectedBy'], $value['contact'], $value['onsetDate'], $value['confirmedDate'], $value['releaseDate'], $value['deceasedDate'], $value['state']);
+            $result[] = new Patient(
+                $value['patient_id'], 
+                $value['sex'], 
+                $value['age'], 
+                $value['country'], 
+                $value['province'], 
+                $value['city'], 
+                $value['infection_case'], 
+                $value['infected_by'], 
+                $value['sympton_onset_date'], 
+                $value['confirmed_date'], 
+                $value['released_date'], 
+                $value['deceased_date'],
+                $value['state']
+            );
         }
 
         return $result;
@@ -179,9 +228,5 @@ class QueryUser extends Model
     public function update_patient()
     {
         
-    }
-
-    public function get_error() {
-        return $this->error;
     }
 }
