add1ad3efebaa1e0a3eecc95431df0f10d160ed3
WiranataLim <74801491+WiranataLim@users.noreply.github.com>
2020-12-29 21:29:06 +0800

    c5 - membuat grafik (#21)
    * add template mvc studture
    
    * adding View
    
    * adding Db
    
    * Update AuthHelper.php
    
    * add views tamplate
    
    * adding views/template
    
    * add Router.php
    
    * add user model
    
    * add QueryUser
    
    * repair template
    
    * repair template
    
    * repair stucture
    
    * repair template
    
    * Mockup v1.0
    
    * C1 - Mockup Website
    
    * Update App.php
    
    * Update layout.php
    
    * picture
    
    * replace
    
    * image
    
    * Update index.php
    
    * change pic
    
    * css home
    
    * query
    
    * update patientcase
    
    * table done
    
    * home page UI done
    
    * repair db data
    
    * remove trash
    
    * Minor Changes
    
    * controllers dan core untuk C3-UI Halaman Grafik
    
    * repair queryPatient and add to page
    
    * add navigation to admin-patient page trough dropdown
    
    * C3-UI Halaman Grafik
    
    * repair minor style and position
    
    * C3- UI Halaman Grafik
    
    * C3 UI Grafik
    
    * c3&c4
    
    * C3 dan C4 Grafik
    
    * C4
    
    * c4
    
    * c4 grafik
    
    * c4 daily change
    
    * C4
    
    * c4 dailyChangeDecease yang hilang
    
    * C4 daily change controller, query data, and view
    
    * merubah huruf depan menjadi upper case dan menambahkan fungsi untuk merubah huruf depan menjadi uppercase sebelum dibaca
    
    * C5 grafik js
    
    * Added Graphic
    
    * fix graphic
    
    Co-authored-by: JuanAnthoniusKusjadi <juan_20200249@unpar.ac.id>
    Co-authored-by: UpekkhaLau <6181801023@student.unpar.ac.id>
    Co-authored-by: DeddyChandra <6181801007@student.unpar.ac.id>
    Co-authored-by: Chris <6181801026@student.unpar.ac.id>
    Co-authored-by: Ramaf <ramafauzifa@gmail.com>
    Co-authored-by: SyahdanRiyantyoPutro <6181801043@student.unpar.ac.id>
    Co-authored-by: florenthiakezia <florent.kezia@gmail.com>
    Co-authored-by: Juan Anthonius Kusjadi <75063337+JuanAnthoniusKusjadi@users.noreply.github.com>
    Co-authored-by: FlorenthiaKezia <64560923+FlorenthiaKezia@users.noreply.github.com>
    Co-authored-by: JosieEsthaliani <josie_esthaliani@yahoo.co.id>
    Co-authored-by: amefedora <ginting.afi21@gmail.com>

--- app/views/pages/grafik/index.php
+++ app/views/pages/grafik/index.php
@@ -64,28 +64,34 @@
                 <div class="card-body">
                     <h3 class="card-title font-weight-bold">Daily Chart Case Growth</h3>
                     <!-- gambar grafik -->
+                    <div>
+                        <canvas id="canvas" width="400" height="200"></canvas>
+                        <p id="p"></p>
+                    </div>
                 </div>
             </div>
 
-            <!-- confirmed and deceased case by gender-->
+            <!-- infected and deceased case by gender-->
             <div class="card-deck">
                 <div class="card shadow p-3 mb-5 bg-white rounded">
                     <center>
                     <div class="card-body">
-                    <h3 class="card-title font-weight-bold">Confirmed Case by Gender</h3>
-                    <!-- gambar grafik -->
-                    <div style="width: 100%; height: 100%">
-                        <p>Chart Here</p>
-                        <canvas id="canvas" width="400" height="100"></canvas>
+                        <h3 class="card-title font-weight-bold">Infected Case by Gender</h3>
+                        <!-- gambar grafik -->
+                        <div>
+                            <canvas id="canvas-2" width="400" height="200"></canvas>
+                        </div>
                     </div>
-                    </div>
-                    </center>
+                </center>
             </div>
             <div class="card shadow p-3 mb-5 bg-white rounded">
                 <center>
                 <div class="card-body">
-                <h3 class="card-title font-weight-bold">Deceased Case by Gender</h3>
-                <!-- gambar grafik -->
+                    <h3 class="card-title font-weight-bold">Deceased Case by Gender</h3>
+                    <!-- gambar grafik -->
+                    <div>
+                        <canvas id="canvas-3" width="400" height="200"></canvas>
+                    </div>
                 </div>
                 </center>
             </div>
@@ -95,8 +101,8 @@
             <div class="card shadow p-3 mb-5 bg-white rounded">
                 <center>
                 <div class="card-body">
-                <h3 class="card-title font-weight-bold">Statistics by Age Interval</h3>
-                <!-- gambar grafik -->
+                    <h3 class="card-title font-weight-bold">Statistics by Age Interval</h3>
+                    <!-- gambar grafik -->
                 </div>
                 </center>
             </div>
@@ -105,11 +111,149 @@
             <div class="card shadow p-3 mb-5 bg-white rounded">
                 <center>
                 <div class="card-body">
-                <h3 class="card-title font-weight-bold">Statistics by Province</h3>
-                <!-- gambar grafik -->
+                    <h3 class="card-title font-weight-bold">Statistics by Province</h3>
+                    <!-- gambar grafik -->
                 </div>
                 </center>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    function getDate(data) {
+        var date = [data.date];
+        return date;
+    }
+
+    function getDailyCount(data) {
+        var count = [data.infectedDaily];
+        return count;
+    }
+
+    function toNumber(x) {
+        return +x;
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("canvas").getContext("2d");
+        new Chart(ctx, myChart);
+
+        var ctx2 = document.getElementById("canvas-2").getContext("2d");
+        new Chart(ctx2, myChart2);
+
+        var ctx3 = document.getElementById("canvas-3").getContext("2d");
+        new Chart(ctx3, myChart3);
+    };
+
+    var dailyCountInfected = new Array();
+    dailyCountInfected = <?php echo json_encode($dailyCountInfected); ?>;
+    var dateLabel = [];
+    var stringCount = [];
+    var dailyCount = [];
+
+    if(dailyCountInfected != '') {
+        dateLabel = dailyCountInfected.map(getDate);
+        stringCount = dailyCountInfected.map(getDailyCount);
+        dailyCount = stringCount.map(toNumber);
+    }
+
+    var myChart = {
+        type: 'line',
+        data: {
+            datasets : [
+                {/* dataset1 */
+                    label: "Daily Growth",
+                    borderColor: window.chartColors.blue,
+                    data: dailyCount,
+                    fill: true,
+                }],
+                labels: dateLabel,
+        },
+        options: {
+            scales: {
+                xAxes : [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date',
+                    },
+                    ticks: {
+                        autoSkip: true,
+                    }
+                }],
+                yAxes : [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Infected(s)'
+                    }
+                }]
+            },
+            responsive: true,
+        }
+            
+    };
+
+    var colors = [
+        window.chartColors.blue,
+        window.chartColors.red,
+    ];
+
+    function getCountGenderInfected(data) {
+        var count = [data.infectedGender];
+        return count;
+    }
+
+    function getCountGenderDeceased(data) {
+        var count = [data.deceasedGender];
+        return count;
+    }
+
+    var countGenderInfected = new Array();
+    var countGenderDeceased = new Array();
+    fullGenderInfected = <?php echo json_encode($countGenderInfected); ?>;
+    fullGenderDeceased = <?php echo json_encode($countGenderDeceased); ?>;
+
+    var stringCountGenderInfected = [];
+    var countGenderInfected = [];
+    var stringCountGenderDeceased = [];
+    var countGenderDeceased = [];
+
+    if(fullGenderInfected != '') {
+        stringCountGenderInfected = fullGenderInfected.map(getCountGenderInfected);
+        countGenderInfected = stringCountGenderInfected.map(toNumber);
+        stringCountGenderDeceased = fullGenderDeceased.map(getCountGenderDeceased);
+        countGenderDeceased = stringCountGenderDeceased.map(toNumber);
+    }
+
+    document.getElementById("p").innerHTML = typeof countGenderInfected[0];
+
+    var myChart2 = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: countGenderInfected,
+                backgroundColor: colors
+            }],
+            labels: ["Male", "Female"],
+        },
+        options: {
+            responsive: true,
+        },
+    };
+
+    var myChart3 = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: countGenderDeceased,
+                backgroundColor: colors
+            }],
+            labels: ["Male", "Female"],
+        },
+        options: {
+            responsive: true,
+        },
+    };
+</script>
